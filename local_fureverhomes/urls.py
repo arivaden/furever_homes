9ed756842e17d201836ae26c276e435f8ade4d19
madison_pierre <mpierre@stedwards.edu>
2022-10-01 18:07:05 -0500

    fixed file structure and updated urls.py

--- local_fureverhomes/urls.py
+++ local_fureverhomes/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from fureverhomes_users_app import views
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.home, name='home')
+    path('', views.home, name='home'),
+    path('/create_account', views.create_account, name='create_account'),
 ]
