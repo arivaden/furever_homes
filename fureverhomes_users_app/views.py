4ac3d6d0bcb729e6d54b5393c9e2b0c540f1f323
madison_pierre <mpierre@stedwards.edu>
2023-01-25 17:30:14 -0600

    inbox test 1

--- fureverhomes_users_app/views.py
+++ fureverhomes_users_app/views.py
@@ -234,7 +234,7 @@ def inbox(request):
         is_co = False
 
     if is_co:
-        owner = FutureOwner.objects.get(user_id=id)
+        owner = Current.objects.get(user_id=id)
         contactable_users = owner.get_contactable_adopters()
     else:
         adopter = FutureOwner.objects.get(user_id= id)
