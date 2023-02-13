bdac8d101ed5327954b3cf7e7dd88461bc22a0e5
madison_pierre <mpierre@stedwards.edu>
2023-02-13 13:17:40 -0600

    updated get_item

--- fureverhomes_users_app/templatetags/dictionary_filters.py
+++ fureverhomes_users_app/templatetags/dictionary_filters.py
@@ -1,6 +1,8 @@
 import django.templatetags
 from django import template
 
+register = template.Library()
 
+@register.filter(name="get_item")
 def get_item(dictionary, key):
 	return dictionary.get(key)
